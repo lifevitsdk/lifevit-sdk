35f271274690738f7f2bbaef94dc899f246bd961
David Casas <dcasas@inmovens.com>
2020-01-17 14:01:27 +0100

    Cambios en báscula.

---
+++ lifevitsdk/src/main/java/es/lifevit/sdk/weightscale/LifevitSDKWeightScaleData.java
@@ -0,0 +1,168 @@
+package es.lifevit.sdk.weightscale;
+
+public class LifevitSDKWeightScaleData {
+
+
+    private double weight;
+    private String unit;
+    private double imc;
+    private double fat;
+    private double fatPercentage;
+    private double water;
+    private double waterPercentage;
+    private double muscle;
+    private double musclePercentage;
+    private double visceral;
+    private double visceralPercentage;
+    private double bone;
+    private double bonePercentage;
+    private double bmr;
+   private double proteinPercentage;
+   private double idealWeight;
+   private double obesityPercentage;
+   private double bodyAge;
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public double getImc() {
+        return imc;
+    }
+
+    public void setImc(double imc) {
+        this.imc = imc;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getFatPercentage() {
+        return fatPercentage;
+    }
+
+    public void setFatPercentage(double fatPercentage) {
+        this.fatPercentage = fatPercentage;
+    }
+
+    public double getWater() {
+        return water;
+    }
+
+    public void setWater(double water) {
+        this.water = water;
+    }
+
+    public double getWaterPercentage() {
+        return waterPercentage;
+    }
+
+    public void setWaterPercentage(double waterPercentage) {
+        this.waterPercentage = waterPercentage;
+    }
+
+    public double getMuscle() {
+        return muscle;
+    }
+
+    public void setMuscle(double muscle) {
+        this.muscle = muscle;
+    }
+
+    public double getMusclePercentage() {
+        return musclePercentage;
+    }
+
+    public void setMusclePercentage(double musclePercentage) {
+        this.musclePercentage = musclePercentage;
+    }
+
+    public double getVisceral() {
+        return visceral;
+    }
+
+    public void setVisceral(double visceral) {
+        this.visceral = visceral;
+    }
+
+    public double getVisceralPercentage() {
+        return visceralPercentage;
+    }
+
+    public void setVisceralPercentage(double visceralPercentage) {
+        this.visceralPercentage = visceralPercentage;
+    }
+
+    public double getBone() {
+        return bone;
+    }
+
+    public void setBone(double bone) {
+        this.bone = bone;
+    }
+
+    public double getBonePercentage() {
+        return bonePercentage;
+    }
+
+    public void setBonePercentage(double bonePercentage) {
+        this.bonePercentage = bonePercentage;
+    }
+
+    public double getBmr() {
+        return bmr;
+    }
+
+    public void setBmr(double bmr) {
+        this.bmr = bmr;
+    }
+
+    public double getProteinPercentage() {
+        return proteinPercentage;
+    }
+
+    public void setProteinPercentage(double protein) {
+        this.proteinPercentage = protein;
+    }
+
+    public double getIdealWeight() {
+        return idealWeight;
+    }
+
+    public void setIdealWeight(double idealWeight) {
+        this.idealWeight = idealWeight;
+    }
+
+    public double getObesityPercentage() {
+        return obesityPercentage;
+    }
+
+    public void setObesityPercentage(double obesityPercentage) {
+        this.obesityPercentage = obesityPercentage;
+    }
+
+    public double getBodyAge() {
+        return bodyAge;
+    }
+
+    public void setBodyAge(double bodyAge) {
+        this.bodyAge = bodyAge;
+    }
+}
